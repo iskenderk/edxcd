7a4b8a2fc81d670fa402c8526e6bec9b2649f799
108005255 <iskender.kaya@ge.com>
2018-12-27 21:22:18 +0300

    Test added

--- cweb/Controllers/HomeController.cs
+++ cweb/Controllers/HomeController.cs
@@ -25,5 +25,11 @@ namespace cweb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult About()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
